69dee2d272f40d32b24646198e945deb2ff0e28c
jfc.me/ <fernandes_punk_@hotmail.com>
2020-03-17 19:58:43 -0400

    Update telegram_bot.sh

--- telegram_bot.sh
+++ telegram_bot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-message="Oi jeferson o computador foi ligado"
+message="hi"
 apiToken=00000000000000000000000000000000000000000000
 userChatId='111111111'
 curl -s   -X POST https://api.telegram.org/bot$apiToken/sendMessage  -d text="$message" -d chat_id=$userChatId
