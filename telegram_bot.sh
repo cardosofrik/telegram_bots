a122bdf7c992c37b28b77760aeebbfd3087f5f71
jfc.me/ <fernandes_punk_@hotmail.com>
2020-03-17 22:34:43 -0400

    Update telegram_bot.sh

--- telegram_bot.sh
+++ telegram_bot.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-message="hi"
+DIA=$(date +%d/%m/%Y)
+message="Hi , I'm active [$DIA]"
 apiToken=00000000000000000000000000000000000000000000
 userChatId='111111111'
-curl -s   -X POST https://api.telegram.org/bot$apiToken/sendMessage  -d text="$message" -d chat_id=$userChatId
+curl -s -o /dev/null  -X POST https://api.telegram.org/bot$apiToken/sendMessage  -d text="$message" -d chat_id=$userChatId
+
